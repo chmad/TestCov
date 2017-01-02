3875b53d8e1b2688fe9219da22d0aa14e1ffca86
chmad <mth.dubois@gmail.com>
2017-01-02 18:13:15 +0000

    Description and test files update

--- tests/testthat/test_Definition.R
+++ tests/testthat/test_Definition.R
@@ -1,34 +1,22 @@
 context("Definition")
 
+# Test for classA ---------------------------------------------------------
 myA = new("ClassA")
 
+#Tests for ClassB --------------------------------------------------------
 myB = new("ClassB", b = 1, objA = myA)
 
-# test_that("ValidClassB throws an error",{
-#     myBTemp = myB
-#     myBTemp@b = -1
-#     expect_error(ValidClassB(myBTemp), "cant be negative")
-#
-# })
+test_that("ValidClassB throws an error",{
+    myBTemp = myB
+    myBTemp@b = -1
+    expect_error(ValidClassB(myBTemp), "cant be negative")
+
+})
 
 
-# # Tests for ClassB --------------------------------------------------------
-# testFct = function(x){TRUE}
-# test_that("fct slot is correct",{
-#    expect_equal(myB@objA@a,testFct)
-#
-# })
-#
 
 
-#myA = new("ChildClassA")
-#
-#
-# test_that("ValidChildClassA throws an error",{
-#   myATemp = myA
-#   myATemp@a = function(){print("hello")}
-#   expect_error(ValidChildClassA(myATemp), "function not equal to prototype" )
-#
-# })
+
+
 
 
